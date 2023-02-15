34b05001e76322428d78fe78dad98ea4d826f914
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2023-02-15 17:34:21 +0700

    Исправлен рандом

--- Digital_models_3/Program.cs
+++ Digital_models_3/Program.cs
@@ -4,7 +4,7 @@ namespace Digital_models_3
 {
     internal class Program
     {
-        static int x_min, x_max, sum_fx = 0, max_fx, chromasome_count = 4;
+        static int x_min, x_max, sum_fx = 0, max_fx, chromasome_count;
 
         static List<int> chromasome_x = new List<int>();
         //static List<int> chromasome_binary = new List<int>();
@@ -21,22 +21,13 @@ namespace Digital_models_3
         {
             using (StreamReader sr = new StreamReader("input.txt"))
             {
+                chromasome_count = int.Parse(sr.ReadLine());
                 x_min = int.Parse(sr.ReadLine());
                 x_max = int.Parse(sr.ReadLine());
             }
 
-            Random rnd = new Random();
-            for (int ctr = 0; ctr < chromasome_count; ctr++)
-            {
-                int x = rnd.Next(x_min, x_max);
-                chromasome_x.Add(x);
-                primary_population.Add(int.Parse(FuncTo2(x)));
-
-                int y = x * x;
-                objective_function.Add(y);
-
-                sum_fx += y;
-            }
+            Random rand = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
+            RandomPoints(rand.Next(), x_min, x_max);
             max_fx = objective_function.Max(x => x);
 
             for(int i = 0; i < chromasome_count; i++)
@@ -46,9 +37,29 @@ namespace Digital_models_3
                 received_copies.Add(Math.Round(expected_copies[i], MidpointRounding.ToEven));
             }
 
-            output();
+            Output();
 
             FirstGeneration firstGeneration = new FirstGeneration();
+        }
+
+        private static void RandomPoints(int seed, int lower, int upper)
+        {
+            Console.WriteLine("Random object, seed = {0}, lower = {1}, upper = {2}:\n", seed, lower, upper);
+            Random randObj = new Random(seed);
+
+            // Generate 4 random integers from the lower to upper bounds.
+            for (int j = 0; j < chromasome_count; j++)
+            {
+                int x = randObj.Next(lower, upper);
+
+                chromasome_x.Add(x);
+                primary_population.Add(int.Parse(FuncTo2(x)));
+
+                int y = x * x;
+                objective_function.Add(y);
+
+                sum_fx += y;
+            }
         }
 
         private static string FuncTo2(int x)
@@ -56,7 +67,7 @@ namespace Digital_models_3
             return Convert.ToString(x, 2);
         }
 
-        private static void output()
+        private static void Output()
         {
             //Console.WriteLine("№ \t начальная популяция \t x \t f(x) \t значение fi(x)/sum[f(x)] \t ожидаемое число копий \t полученных копий");
             for (int i = 0; i < chromasome_count; i++)
